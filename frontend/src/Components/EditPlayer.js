554f4c35c1d743dba258d845609209ed9bf1670c
BransonCoblentz <bdc30468@uga.edu>
2023-04-25 15:03:44 -0400

    Implemented EditPlayer (need to fix getById)

--- frontend/src/Components/EditPlayer.js
+++ frontend/src/Components/EditPlayer.js
@@ -1,30 +1,69 @@
 import './EditPlayer.css'
 import {Link, useNavigate, useParams} from "react-router-dom";
+import {useState, useEffect} from 'react';
 import axios from 'axios'
 
-function EditPlayer(props) {
+function EditPlayer() {
     const navigate = useNavigate();
     const {id} = useParams();
+    const [name, setName] = useState('');
+    const [stats, setStats] = useState('');
+    const [position, setPosition] = useState('');
+    const [image, setImage] = useState('');
+
+        useEffect(() => {
+            axios
+                .get(`http://localhost:3001/api/items/${id}`)
+                .then((res) => {
+                        setName(res.data.name);
+                        setStats(res.data.stats);
+                        setPosition(res.data.position);
+                        setImage(res.data.image);
+                })
+                .catch((err) => {
+                    console.log('Error from EditPlayer' , err);
+                })
+        }, [id])
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const data = {
+            name,
+            stats,
+            position,
+            image,
+        }
+        axios  
+            .put(`http://localhost:3001/api/items/${id}`, data)
+            .then((res) => {
+                navigate('/');
+            })
+            .catch((err) => {
+                console.log('Error in EditPlayer', err);
+            })
+    }
     return(
         <div>
             <h2>Edit Player</h2>
         <form className='form'>
             <label className='name'>
                 Name:
-                <input type='text'/>
+                <input type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
             </label>
             <label className='position'>
                 Position:
-                <input type='text'/>
+                <input type='text' value={position} onChange={(e)=>setPosition(e.target.value)}/>
             </label>
             <label className='stats'>
                 Stats:
-                <input type='text'/>
+                <input type='text' value={stats} onChange={(e)=>setStats(e.target.value)}/>
             </label>
             <label className='image'>
                 Image:
-                <input type='text'/>
+                <input type='text' value={image} onChange={(e)=>setImage(e.target.value)}/>
             </label>
+            <button onClick={onSubmit} className='submit' type='submit'>Submit</button>
         </form>
         </div>
         )
